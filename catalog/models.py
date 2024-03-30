7795917372ef08d9e9b9f07d53b96e40bb9d79a3
Екатерина Кудряшова <katyushka555@yandex.ru>
2024-03-30 12:41:14 +0400

    Перемещение fixtures, доработка

--- catalog/models.py
+++ catalog/models.py
@@ -3,24 +3,6 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.IntegerField(verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена')
-    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
-    updated_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True)
-
-    def __str__(self):
-        return f"{self.name} ({self.category})"
-
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-        ordering = ('name',)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
@@ -31,4 +13,22 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
         ordering = ('name',)
